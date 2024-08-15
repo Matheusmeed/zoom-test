e959a465b1caeeac56aeafa5fcf6fc22c5702c6c
Matheus <mms.medeiros12@gmail.com>
2024-08-15 15:53:45 -0300

    fix: position error

--- src/components/ZoomableComponent/index.tsx
+++ src/components/ZoomableComponent/index.tsx
@@ -3,7 +3,10 @@ import { Container, ZoomButtonsDiv } from './styles';
 
 const ZoomableComponent = () => {
   return (
-    <TransformWrapper initialScale={1}>
+    <TransformWrapper
+      initialScale={1}
+      alignmentAnimation={{ sizeX: 0, sizeY: 0 }}
+    >
       {({ zoomIn, zoomOut, resetTransform }) => (
         <>
           <ZoomButtonsDiv>
