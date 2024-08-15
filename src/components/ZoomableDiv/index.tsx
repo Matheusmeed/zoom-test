9383daa0e1aa8fda3881905a226729246f458730
Matheus <mms.medeiros12@gmail.com>
2024-08-15 10:26:53 -0300

    feat: adding property image, container and adding zoom pan pinch lib

---
+++ src/components/ZoomableDiv/index.tsx
@@ -0,0 +1,11 @@
+import { Container, PropertyImage } from './styles';
+
+const ZoomableDiv = () => {
+  return (
+    <Container>
+      <PropertyImage src='/images/property.jpg' alt='minimap' />
+    </Container>
+  );
+};
+
+export default ZoomableDiv;
---
+++ src/components/ZoomableDiv/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  width: 50%;
+  height: 50%;
+  overflow: hidden;
+`;
+
+export const PropertyImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
