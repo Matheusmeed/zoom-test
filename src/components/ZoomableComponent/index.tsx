c31459df7698b902a52bf8362868bdce81b3bdc1
Matheus <mms.medeiros12@gmail.com>
2024-08-15 16:44:25 -0300

    feat: adding minimap and fixing positions

--- src/components/ZoomableComponent/index.tsx
+++ src/components/ZoomableComponent/index.tsx
@@ -1,42 +1,37 @@
-import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
-import { Container, ZoomButtonsDiv } from './styles';
+import {
+  TransformWrapper,
+  TransformComponent,
+  MiniMap,
+} from 'react-zoom-pan-pinch';
+import { Container, ZoomButtonsDiv, MinimapContainer } from './styles';
 
 const ZoomableComponent = () => {
   return (
     <TransformWrapper
       initialScale={1}
       alignmentAnimation={{ sizeX: 0, sizeY: 0 }}
+      smooth
+      centerZoomedOut
     >
       {({ zoomIn, zoomOut, resetTransform }) => (
         <>
           <ZoomButtonsDiv>
-            <button
-              onClick={() => {
-                zoomIn();
-              }}
-            >
-              +
-            </button>
-            <button
-              onClick={() => {
-                zoomOut();
-              }}
-            >
-              -
-            </button>
-            <button
-              onClick={() => {
-                resetTransform();
-              }}
-            >
-              Reset
-            </button>
+            <button onClick={() => zoomIn()}>+</button>
+            <button onClick={() => zoomOut()}>-</button>
+            <button onClick={() => resetTransform()}>Reset</button>
           </ZoomButtonsDiv>
+
           <TransformComponent>
             <Container>
-              <img src='/assets/images/property.jpg' alt='property' />
+              <img src='/assets/images/property2.webp' alt='property' />
             </Container>
           </TransformComponent>
+
+          <MinimapContainer>
+            <MiniMap borderColor='red' style={{ border: '1px solid white' }}>
+              <img src='/assets/images/property2.webp' alt='property mini' />
+            </MiniMap>
+          </MinimapContainer>
         </>
       )}
     </TransformWrapper>
--- src/components/ZoomableComponent/styles.ts
+++ src/components/ZoomableComponent/styles.ts
@@ -22,9 +22,32 @@ export const ZoomButtonsDiv = styled.div`
   gap: 10px;
 
   button {
+    cursor: pointer;
     background-color: white;
     border-radius: 6px;
     padding: 6px 10px;
     border: 2px solid black;
+    user-select: none;
+  }
+`;
+
+export const MinimapContainer = styled.div`
+  user-select: none;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  z-index: 1;
+  background-color: rgba(255, 255, 255, 0.5);
+  border: 2px solid black;
+  border-radius: 8px;
+  overflow: hidden;
+
+  img {
+    height: 100%;
+    width: 100%;
+  }
+
+  .rzpp-preview {
+    border: 2px solid red !important;
   }
 `;
